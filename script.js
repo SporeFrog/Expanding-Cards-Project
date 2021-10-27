5494e566c03708ee2913606acce8550e65e32a49
Aaron <anaugustchill@gmail.com>
2021-10-27 14:12:01 -0400

    commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
+var numberOfPanels = document.querySelectorAll(".panel").length;
+
 function switchActive (){
+  for(var i = 0; i < numberOfPanels; i++ ){
+    document.querySelectorAll(".panel")[i].classList.remove("active");
+  }
+  
   if(this.classList.contains("active")){
     this.classList.remove("active");
-
   } else {
     this.classList.add("active");
   }
   // var otherPanels = document.querySelectorAll()[]
 }
-var numberOfPanels = document.querySelectorAll(".panel").length;
+
 for(var i = 0; i < numberOfPanels; i++ ){
   document.querySelectorAll(".panel")[i].addEventListener("click", switchActive);
 }
